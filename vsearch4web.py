ea68b83a3545960661716a59cf33ca3e6d2c45fc
Cyril Aguvasu <aguvasucyril@gmail.com>
2023-11-28 16:39:54 +0300

    added context manager class on db handling

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,36 +1,23 @@
 from flask import Flask, render_template, request, escape
 from vsearch import search4letters
+from DBcm import UseDatabase
 
 app = Flask(__name__)
 
-def log_request(req: 'flask_request', res: str) ->None:
-    """Log the web request in a database"""
-    dbconfig = {'host':'127.0.0.1',
+app.config['dbconfig'] = {'host':'127.0.0.1',
                 'user':'root',
                 'password':'@!kumamake254!@',
                 'database':'vsearchlogDB',}
-    
-    import mysql.connector
 
-    conn = mysql.connector.connect(**dbconfig)
-    cursor = conn.cursor()
-    _SQL = """insert into log
-        (phrase, letters, ip, browser_string, results)
-        values
-        (%s, %s, %s, %s, %s)"""
-    if not req.user_agent.browser:
-        browser_string = ''
-    else:
-        browser_string = req.user_agent.browser
-
-    cursor.execute(_SQL, (req.form['phrase'], 
-                          req.form['letters'], 
-                          req.remote_addr,
-                          browser_string,
-                          res, ))
-    conn.commit()
-    cursor.close()
-    conn.close()
+def log_request(req: 'flask_request', res: str) ->None:
+    """Log the web request in a database"""
+    with UseDatabase(app.config['dbconfig']) as cursor:
+        _SQL = """insert into log (phrase, letters, ip, browser_string, results) values (%s, %s, %s, %s, %s)"""
+        if not req.user_agent.browser:
+            browser_string = ''
+        else:
+            browser_string = req.user_agent.browser
+        cursor.execute(_SQL, (req.form['phrase'], req.form['letters'], req.remote_addr,browser_string,res, ))
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
@@ -53,13 +40,12 @@ def entry_page() -> 'html':
 
 @app.route('/viewlog')
 def view_the_log()-> 'html':
-    contents = []
-    with open('vsearch.log') as log:
-        for line in log:
-            contents.append([])
-            for item in line.split('|'):
-                contents[-1].append(escape(item))
-    titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results');
+    with UseDatabase(app.config['dbconfig']) as cursor:
+        _SQL = """select phrase, letters, ip, browser_string, results from log"""
+        cursor.execute(_SQL)
+        contents = cursor.fetchall()
+    
+    titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results');
     return render_template('viewlog.html',
                            the_title='View Log',
                            the_row_titles=titles,
